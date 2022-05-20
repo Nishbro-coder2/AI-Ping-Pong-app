8cc5eb22de117a822cc1115936419609c0d92e53
Nishbro-coder2 <98819146+Nishbro-coder2@users.noreply.github.com>
2022-05-20 11:41:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,15 +20,38 @@ var ball = {
     dx:3,
     dy:3
 }
+rightWristX = 0;
+rightWristY = 0;
+score_rightWrist = 0;
+game_status = "";
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video , modelLoaded);
+  poseNet.on('pose' , gotPoses);
+}
+
+function gotPoses(results){
+  if(results.length > 0){
+    console.log(results);
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    score_rightWrist = results[0].pose.keypoints[10].score;
+  }
+}
+
+function modelLoaded(){
+  console.log("Model Loaded!!");
 }
 
 
 function draw(){
 
- background(0); 
+ background(0);
+ image(video,0,0,700,600); 
 
  fill("black");
  stroke("black");
